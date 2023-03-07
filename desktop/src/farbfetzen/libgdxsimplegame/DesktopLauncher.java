73f0191aa5683ed8c459df2e224e1b93d27db920
farbfetzen <bastihz.dev@posteo.de>
2023-03-07 16:41:56 +0100

    Clean up Java files
    - indent using spaces
    - remove unused import
    - remove comment pertaining macOS
    - use custom rules for empty lines
    - make some variables final

--- desktop/src/farbfetzen/libgdxsimplegame/DesktopLauncher.java
+++ desktop/src/farbfetzen/libgdxsimplegame/DesktopLauncher.java
@@ -2,14 +2,14 @@ package farbfetzen.libgdxsimplegame;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import farbfetzen.libgdxsimplegame.SimpleGame;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("libGDX-Simple-Game");
-		new Lwjgl3Application(new SimpleGame(), config);
-	}
+
+    public static void main(final String[] arg) {
+        final Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("libGDX-Simple-Game");
+        new Lwjgl3Application(new SimpleGame(), config);
+    }
+
 }
